53a422ed3d3eae0a0930e55ab124f65837f02b1a
Aurelien <Aurelien@172.17.75.121>
2017-05-16 16:41:25 +0200

    Form order final

--- Scrum/src/scrum/Interface/FormOrder.java
+++ Scrum/src/scrum/Interface/FormOrder.java
@@ -25,8 +25,8 @@ import scrum.noyau.Specie;
 import scrum.noyau.SpecieCategory;
 
 /**
- *
- * @author Delphine
+ * Class to represent the form to add an Order
+ * @author Delphine & Aurelien
  */
 public class FormOrder extends JPanel {
     
@@ -44,31 +44,40 @@ public class FormOrder extends JPanel {
     private JButton validateForm;
     private JButton butRetour;
 
+    /**
+     * The constructor of the class
+     * @param menu The main menu of our interface, to allow the redirection
+     * @param listAnalysis All the analysys available on the application
+     * @param listCategory All the categories available on the application
+     */
     public FormOrder(MenuPrincipal menu,ArrayList<Analysis> listAnalysis, ArrayList<SpecieCategory> listCategory){
     	this.setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
-        myMenu = menu;
-        controleur = new CtrlFormOrder(this);
+    	this.myMenu = menu;
+        this.controleur = new CtrlFormOrder(this);
         this.listAnalysis = listAnalysis;
         
         analysis = new JComboBox<Analysis>();
         
+        // Title of the panel
         JLabel labelTitle = new JLabel("New order");
         labelTitle.setFont(new Font(Font.DIALOG,Font.BOLD,25));
         labelTitle.setHorizontalAlignment(SwingConstants.CENTER);
         
+        // The field for the name
         nameOrder = new JTextField(20);
         JPanel panelField1 = new JPanel();
         panelField1.setLayout(new FlowLayout(FlowLayout.LEFT));
         panelField1.add(new JLabel("Name of the order"));
         panelField1.add(nameOrder);
         
+        // The field for the number of sample
         numberSample = new JTextField(20);
         JPanel panelField2 = new JPanel();
         panelField2.setLayout(new FlowLayout(FlowLayout.LEFT));
         panelField2.add(new JLabel("Number of samples"));
         panelField2.add(numberSample);
         
-        
+        // Creation of the JCombobox for all the category
         category = new JComboBox<SpecieCategory>();
         category.addActionListener(
         		new ActionListener(){
@@ -84,7 +93,7 @@ public class FormOrder extends JPanel {
         panelComboCategory.add(new JLabel("Category"));
         panelComboCategory.add(category);
         
-        
+        // Creation of the JCombobox for all the species
         species = new JComboBox<Specie>();
         species.addActionListener(
         		new ActionListener(){
@@ -100,14 +109,13 @@ public class FormOrder extends JPanel {
         panelComboSpecie.add(new JLabel("Specie"));
         panelComboSpecie.add(species);
         
+        // Creation of the JCombobox for all the analysis
         JPanel panelComboAnalysis = new JPanel();
         panelComboAnalysis.setLayout(new FlowLayout(FlowLayout.LEFT));
         panelComboAnalysis.add(new JLabel("Analysis"));
         panelComboAnalysis.add(analysis);
         
-        
-        JPanel panelForm = new JPanel(new FlowLayout(LEFT));
-        
+        // Initialization of the field
         int compteur = 0;
         for (SpecieCategory specieCategory : listCategory) {
         	if(compteur == 0){
@@ -118,32 +126,35 @@ public class FormOrder extends JPanel {
         	category.addItem(specieCategory);
 		}
         
+        // Creation of the button to return to the main menu and the validate button
         JPanel panButValid = new JPanel();
         validateForm = new JButton("Valider");
         validateForm.setPreferredSize(new Dimension(200,20));
         validateForm.setFont(new Font(Font.DIALOG,Font.BOLD,17));
         validateForm.addActionListener(controleur);
         panButValid.add(validateForm);
-        
         JPanel panButRetour = new JPanel();
         butRetour = new JButton("Retour");
         butRetour.addActionListener(controleur);
         panButRetour.add(butRetour);
-        
         JPanel panButtons = new JPanel(new GridLayout(1,2));
         panButtons.add(panButRetour);
         panButtons.add(panButValid);
         
+        // Add all the components
         this.add(labelTitle);
         this.add(panelField1);
         this.add(panelComboCategory);
         this.add(panelComboSpecie);
         this.add(panelComboAnalysis);
         this.add(panelField2);
-        
         this.add(panButtons);
     }
 
+    /**
+     * Function to allow the modification of the analysis according to the specie
+     * @param specieChoose The specie choosen
+     */
 	private void majAnalysis(Specie specieChoose) {
 		analysis.removeAllItems();
 		for (Analysis analys : listAnalysis) {
@@ -152,6 +163,10 @@ public class FormOrder extends JPanel {
 		}
 	}
 
+	/**
+	 * Function to allow the modification of the species according to the category
+	 * @param categoryChoose The category choosen
+	 */
 	private void majSpecies(SpecieCategory categoryChoose) {
 		species.removeAllItems();
 		for (Specie spec : categoryChoose.getSpecies()) {
@@ -159,40 +174,55 @@ public class FormOrder extends JPanel {
 		}
 	}
         
-        public MenuPrincipal getLeMenuP(){
-            return myMenu;
-        }
-        
-        public JButton getButValider(){
-            return validateForm;
-        }
-        
-        public JButton getButRetour(){
-            return butRetour;
-        }
+	/**
+	 * Function to get the main menu
+	 * @return The main menu
+	 */
+    public MenuPrincipal getLeMenuP(){
+        return myMenu;
+    }
+    
+    /**
+     * Function to get the validating button
+     * @return The button to validate
+     */
+    public JButton getButValider(){
+        return validateForm;
+    }
+    
+    /**
+     * Function to get the return button
+     * @return The button to return to the main menu
+     */
+    public JButton getButRetour(){
+        return butRetour;
+    }
 
-		public void validationForm() {
-			if(numberSample.getText().isEmpty() || nameOrder.getText().isEmpty()){
-				JOptionPane boiteDial = new JOptionPane();
-                boiteDial.showMessageDialog(null, "One field is empty", "Create order", JOptionPane.INFORMATION_MESSAGE);
-			}
-			else{
-				try
-		        {
-		            int i= Integer.parseInt(numberSample.getText());
-		            if(i <= 0){
-						JOptionPane boiteDial = new JOptionPane();
-		                boiteDial.showMessageDialog(null, "The number of analysis needs to be upper than 0", "Create order", JOptionPane.INFORMATION_MESSAGE);
-					}
-					else
-						myMenu.afficheMenuPrincipal();
-		        }
-		        catch(NumberFormatException e)
-		        {
-		        	JOptionPane boiteDial = new JOptionPane();
-	                boiteDial.showMessageDialog(null, "The number of analysis needs to be a numeric value", "Create order", JOptionPane.INFORMATION_MESSAGE);
-		        } 
-			}
-			
+    /**
+     * Function to validate all the forms according to the specification
+     */
+	public void validationForm() {
+		if(numberSample.getText().isEmpty() || nameOrder.getText().isEmpty()){
+			JOptionPane boiteDial = new JOptionPane();
+            boiteDial.showMessageDialog(null, "One field is empty", "Create order", JOptionPane.INFORMATION_MESSAGE);
 		}
+		else{
+			try
+	        {
+	            int i= Integer.parseInt(numberSample.getText());
+	            if(i <= 0){
+					JOptionPane boiteDial = new JOptionPane();
+	                boiteDial.showMessageDialog(null, "The number of analysis needs to be upper than 0", "Create order", JOptionPane.INFORMATION_MESSAGE);
+				}
+				else
+					myMenu.afficheMenuPrincipal();
+	        }
+	        catch(NumberFormatException e)
+	        {
+	        	JOptionPane boiteDial = new JOptionPane();
+                boiteDial.showMessageDialog(null, "The number of analysis needs to be a numeric value", "Create order", JOptionPane.INFORMATION_MESSAGE);
+	        } 
+		}
+		
+	}
 }
