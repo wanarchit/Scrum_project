cbb40fde020912c178c3315a0e1d291f98cf3d82
Delphine <Delphine@172.17.79.100>
2017-05-16 16:24:01 +0200

    Controleur specie OK DP

--- Scrum/src/scrum/Interface/FormSpecies.java
+++ Scrum/src/scrum/Interface/FormSpecies.java
@@ -1,14 +1,13 @@
 package scrum.Interface;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
-import java.util.ArrayList;
-
+import java.awt.GridLayout;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -23,16 +22,13 @@ public class FormSpecies extends JPanel {
     private JComboBox<SpecieCategory> combo;
     private CtrlFormSpecie controleur;
     private JButton valider;
+    private JButton retour;
 
     public FormSpecies(MenuPrincipal theMenu) {
-        
-        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        this.setAlignmentX(LEFT_ALIGNMENT);
         
         controleur = new CtrlFormSpecie(this);
         menu = theMenu;
        
-
         JLabel headTitle = new JLabel("New specie");
         headTitle.setFont(new java.awt.Font(Font.DIALOG, Font.BOLD, 20));
         headTitle.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
@@ -54,12 +50,29 @@ public class FormSpecies extends JPanel {
         }
         category.add(titleCategory);
         category.add(combo);
-        valider = new JButton("Validate");
+        
+        JPanel form = new JPanel(new BorderLayout());
+        form.add(nom, BorderLayout.NORTH);
+        form.add(category, BorderLayout.CENTER);
+        
+        JPanel panButValid = new JPanel();
+        valider = new JButton("Validation");
         valider.addActionListener(controleur);
-
-        this.add(nom);
-        this.add(category);
-        this.add(valider);
+        panButValid.add(valider);
+        
+        JPanel panButRetour = new JPanel();
+        retour = new JButton("Retour");
+        retour.addActionListener(controleur);
+        panButRetour.add(retour);
+        
+        JPanel panButtons = new JPanel(new GridLayout(1,2));
+        panButtons.add(panButRetour);
+        panButtons.add(panButValid);
+        
+        this.setLayout(new BorderLayout());
+        this.add(headTitle,BorderLayout.NORTH);
+        this.add(form, BorderLayout.WEST);
+        this.add(panButtons, BorderLayout.SOUTH);
     }
 
     public JTextField getValFieldNom(){
@@ -74,6 +87,10 @@ public class FormSpecies extends JPanel {
         return valider;
     }
     
+    public JButton getButRetour(){
+        return retour;
+    }
+        
     public MenuPrincipal getLeMenu(){
         return menu;
     }
