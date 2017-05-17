33f2610798eed89b393bc9fb52fda534fe7abf98
Paul <Paul@DESKTOP-1U9DTNA>
2017-05-17 16:15:12 +0200

    Modif menu principal

---
+++ Scrum/src/scrum/Interface/SelectMicroplate.java
@@ -0,0 +1,25 @@
+package scrum.Interface;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Paul
+ */
+public class SelectMicroplate extends JPanel{
+    
+    public SelectMicroplate(){
+        
+        //
+        
+        JPanel panChoixMP = new JPanel();
+        panChoixMP.add(new JLabel("Microplaque : "));
+        
+        
+        //panChoixMP.add();
+        
+        //this.add();
+        
+    }
+}
