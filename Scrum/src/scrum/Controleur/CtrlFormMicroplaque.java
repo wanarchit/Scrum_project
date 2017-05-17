2242aae4bf1e6ce061cde973474653f3d5e87257
Delphine <Delphine@172.17.79.100>
2017-05-17 11:02:57 +0200

    microplaque DP

---
+++ Scrum/src/scrum/Controleur/CtrlFormMicroplaque.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scrum.Controleur;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import scrum.Interface.FormMicroplaque;
+
+/**
+ *
+ * @author Delphine
+ */
+public class CtrlFormMicroplaque implements ActionListener{
+    
+        private FormMicroplaque myForm;
+
+    public CtrlFormMicroplaque(FormMicroplaque form) {
+        myForm = form;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+    }
+
+    
+}
--- Scrum/src/scrum/Interface/FormMicroplaque.java
+++ Scrum/src/scrum/Interface/FormMicroplaque.java
@@ -1,6 +1,9 @@
 package scrum.Interface;
 
+import java.awt.Font;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import scrum.Controleur.CtrlFormCustomer;
 
 /**
  *
@@ -8,4 +11,18 @@ import javax.swing.JPanel;
  */
 public class FormMicroplaque extends JPanel {
     
+    MenuPrincipal myMenu;
+    CtrlFormMicroplaque controleur; //A CREER
+    
+    
+    public FormMicroplaque(MenuPrincipal leMenu){
+        myMenu = leMenu;
+        controleur = new CtrlFormCustomer(this);
+        
+        // Title
+        JLabel headLabel = new JLabel("New Microplaque");
+        headLabel.setFont(new java.awt.Font(Font.DIALOG,Font.BOLD,20));
+        headLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+
+    }
 }
