cd39e35affc3adb46ad0fb6f2c53238b5ba2b37b
Delphine <Delphine@172.17.79.100>
2017-05-18 10:49:34 +0200

    microplaque DP

--- Scrum/src/scrum/Interface/FormMicroplaque.java
+++ Scrum/src/scrum/Interface/FormMicroplaque.java
@@ -7,6 +7,7 @@ import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.util.ArrayList;
 import javax.swing.BorderFactory;
 import javax.swing.border.Border;
 import javax.swing.JButton;
@@ -24,20 +25,21 @@ import scrum.noyau.Sample;
  */
 public class FormMicroplaque extends JPanel {
 
-    MenuPrincipal myMenu;
-    CtrlFormMicroplaque controleur;
-    JLabel butSample1;
-    JLabel butSample2;
-    JLabel butSample3;
-    JLabel butSample4;
-    JLabel butSample5;
-    JLabel butSample6;
-    JLabel butSample7;
-    JLabel butSample8;
-    JButton validateForm;
-    JButton butRetour;
-    JTextField nameMicro;
-    JButton ajouter;
+    private MenuPrincipal myMenu;
+    private CtrlFormMicroplaque controleur;
+    private JLabel butSample1;
+    private JLabel butSample2;
+    private JLabel butSample3;
+    private JLabel butSample4;
+    private JLabel butSample5;
+    private JLabel butSample6;
+    private JLabel butSample7;
+    private JLabel butSample8;
+    private JButton validateForm;
+    private JButton butRetour;
+    private JTextField nameMicro;
+    private JButton ajouter;
+    private JComboBox<Sample> listeSamp;
 
     public FormMicroplaque(MenuPrincipal leMenu) {
         myMenu = leMenu;
@@ -47,106 +49,128 @@ public class FormMicroplaque extends JPanel {
         JLabel headLabel = new JLabel("New Microplaque");
         headLabel.setFont(new java.awt.Font(Font.DIALOG, Font.BOLD, 20));
         headLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        
+
         Border border = BorderFactory.createLineBorder(Color.BLACK, 1);
-        
+
         JLabel labName = new JLabel("Name of microplate : ");
         nameMicro = new JTextField(20);
         JPanel pan1 = new JPanel();
         pan1.add(labName);
         pan1.add(nameMicro);
 
-        butSample1 = new JLabel ("Sample 1");
+        butSample1 = new JLabel("Sample 1");
         butSample1.setBorder(border);
         butSample1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        butSample2 = new JLabel ("Sample 2");
+        butSample2 = new JLabel("Sample 2");
         butSample2.setBorder(border);
         butSample2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        butSample3 = new JLabel ("Sample 3");
+        butSample3 = new JLabel("Sample 3");
         butSample3.setBorder(border);
         butSample3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        butSample4 = new JLabel ("Sample 4");
+        butSample4 = new JLabel("Sample 4");
         butSample4.setBorder(border);
         butSample4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        butSample5 = new JLabel ("Sample 5");
+        butSample5 = new JLabel("Sample 5");
         butSample5.setBorder(border);
         butSample5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        butSample6 = new JLabel ("Sample 6");
+        butSample6 = new JLabel("Sample 6");
         butSample6.setBorder(border);
         butSample6.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        butSample7 = new JLabel ("Sample 7");
+        butSample7 = new JLabel("Sample 7");
         butSample7.setBorder(border);
         butSample7.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        butSample8 = new JLabel ("Sample 8");
+        butSample8 = new JLabel("Sample 8");
         butSample8.setBorder(border);
         butSample8.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-       
-        
-        JPanel panelGeneral = new JPanel (new GridBagLayout());
+
+        JPanel panelGeneral = new JPanel(new GridBagLayout());
         GridBagConstraints centre = new GridBagConstraints();
-        
+
         centre.gridx = 0;
         centre.gridy = 0;
-        butSample1.setPreferredSize(new Dimension(100,75));
-        panelGeneral.add(butSample1,centre);
-        
+        butSample1.setPreferredSize(new Dimension(100, 75));
+        panelGeneral.add(butSample1, centre);
+
         centre.gridx = 1;
-        centre.gridy = 0; 
-        butSample2.setPreferredSize(new Dimension(100,75));
-        panelGeneral.add(butSample2,centre);
-        
+        centre.gridy = 0;
+        butSample2.setPreferredSize(new Dimension(100, 75));
+        panelGeneral.add(butSample2, centre);
+
         centre.gridx = 2;
         centre.gridy = 0;
-        butSample3.setPreferredSize(new Dimension(100,75));
-        panelGeneral.add(butSample3,centre);
-        
+        butSample3.setPreferredSize(new Dimension(100, 75));
+        panelGeneral.add(butSample3, centre);
+
         centre.gridx = 3;
-        centre.gridy = 0;    
-        butSample4.setPreferredSize(new Dimension(100,75));
-        panelGeneral.add(butSample4,centre);
-        
+        centre.gridy = 0;
+        butSample4.setPreferredSize(new Dimension(100, 75));
+        panelGeneral.add(butSample4, centre);
+
         centre.gridx = 0;
-        centre.gridy = 1;  
-        butSample5.setPreferredSize(new Dimension(100,75));
-        panelGeneral.add(butSample5,centre);
-        
+        centre.gridy = 1;
+        butSample5.setPreferredSize(new Dimension(100, 75));
+        panelGeneral.add(butSample5, centre);
+
         centre.gridx = 1;
-        centre.gridy = 1;   
-        butSample6.setPreferredSize(new Dimension(100,75));
-        panelGeneral.add(butSample6,centre);
-        
+        centre.gridy = 1;
+        butSample6.setPreferredSize(new Dimension(100, 75));
+        panelGeneral.add(butSample6, centre);
+
         centre.gridx = 2;
-        centre.gridy = 1;  
-        butSample7.setPreferredSize(new Dimension(100,75));
-        panelGeneral.add(butSample7,centre);
-        
+        centre.gridy = 1;
+        butSample7.setPreferredSize(new Dimension(100, 75));
+        panelGeneral.add(butSample7, centre);
+
         centre.gridx = 3;
-        centre.gridy = 1;    
-        butSample8.setPreferredSize(new Dimension(100,75));
-        panelGeneral.add(butSample8,centre);
-        
+        centre.gridy = 1;
+        butSample8.setPreferredSize(new Dimension(100, 75));
+        panelGeneral.add(butSample8, centre);
+
         JPanel panelDroite = new JPanel(new BorderLayout());
         panelDroite.add(pan1, BorderLayout.NORTH);
         panelDroite.add(panelGeneral, BorderLayout.CENTER);
-        
-        
+
         //Partie gauche
         JLabel lesSamp = new JLabel("The samples");
-        JComboBox listeSamp = new JComboBox();
-        for (Order or : myMenu.getListOrder()){
-            for (Sample samp : or.getSamples()){
-                listeSamp.addItem(samp.getIdSample());
+        ArrayList<Order> myListOrder = myMenu.getListOrder();
+        ArrayList<Sample> myListSample = new ArrayList();
+
+        for (Order or : myListOrder) {
+            for (Sample samp : or.getSamples()) {
+                myListSample.add(samp);
             }
         }
-        
+
+        ArrayList<Sample> sampPrio = new ArrayList();
+        ArrayList<Sample> sampAutre = new ArrayList();
+
+        for (Sample samp : myListSample) {
+            //ResultStatut 
+            if (samp.getResults().size() > 0) {
+                if (samp.getResults().get((samp.getResults().size())-1).getStatus().equals("UNREADABLE")) {
+                    sampPrio.add(samp);
+                }
+            } else {
+                sampAutre.add(samp);
+            }
+        }
+
+        listeSamp = new JComboBox<>();
+        for (Sample samp : sampPrio) {
+            listeSamp.addItem(samp);
+        }
+
+        for (Sample samp : sampAutre) {
+            listeSamp.addItem(samp);
+        }
+
         ajouter = new JButton("Add the sample");
 
-        JPanel panelGauche = new JPanel (new BorderLayout());
+        JPanel panelGauche = new JPanel(new BorderLayout());
         panelGauche.add(lesSamp, BorderLayout.NORTH);
-        panelGauche.add(listeSamp, BorderLayout.CENTER);    
-        panelGauche.add(ajouter, BorderLayout.SOUTH); 
-        
-        
+        panelGauche.add(listeSamp, BorderLayout.CENTER);
+        panelGauche.add(ajouter, BorderLayout.SOUTH);
+
         // Creation of the button to return to the main menu and the validate button
         JPanel panButValid = new JPanel();
         validateForm = new JButton("Valider");
@@ -161,55 +185,59 @@ public class FormMicroplaque extends JPanel {
         JPanel panButtons = new JPanel(new GridLayout(1, 2));
         panButtons.add(panButRetour);
         panButtons.add(panButValid);
-        
+
         this.setLayout(new BorderLayout());
-        this.add(headLabel,BorderLayout.NORTH);
-        this.add(panelDroite,BorderLayout.CENTER);
-        this.add(panelGauche,BorderLayout.WEST);
-        this.add(panButtons,BorderLayout.SOUTH);
+        this.add(headLabel, BorderLayout.NORTH);
+        this.add(panelDroite, BorderLayout.CENTER);
+        this.add(panelGauche, BorderLayout.WEST);
+        this.add(panButtons, BorderLayout.SOUTH);
     }
-    
-    public MenuPrincipal getLeMenuP(){
+
+    public MenuPrincipal getLeMenuP() {
         return myMenu;
     }
-    
-    public JButton getButValid(){
+
+    public JButton getButValid() {
         return validateForm;
     }
-    
-    public JButton getButRetour(){
+
+    public JButton getButRetour() {
         return butRetour;
     }
-    
-    public JLabel getButS1(){
+
+    public JLabel getButS1() {
         return butSample1;
-    }    
-    
-    public JLabel getButS2(){
+    }
+
+    public JLabel getButS2() {
         return butSample2;
-    }    
-    
-    public JLabel getButS3(){
+    }
+
+    public JLabel getButS3() {
         return butSample3;
-    }    
-    
-    public JLabel getButS4(){
+    }
+
+    public JLabel getButS4() {
         return butSample4;
-    }    
-    
-    public JLabel getButS5(){
+    }
+
+    public JLabel getButS5() {
         return butSample5;
-    }    
-    
-    public JLabel getButS6(){
+    }
+
+    public JLabel getButS6() {
         return butSample6;
-    }    
-    
-    public JLabel getButS7(){
+    }
+
+    public JLabel getButS7() {
         return butSample7;
-    }    
-    
-    public JLabel getButS8(){
+    }
+
+    public JLabel getButS8() {
         return butSample8;
-    }  
+    }
+
+    public JButton getButAdd() {
+        return ajouter;
+    }
 }
--- Scrum/src/scrum/noyau/Sample.java
+++ Scrum/src/scrum/noyau/Sample.java
@@ -3,95 +3,110 @@ package scrum.noyau;
 import java.util.*;
 
 /**
- * Describes a sample to be analysed
- * Here, very partial implementation. We can only add a result, and recover the last result
+ * Describes a sample to be analysed Here, very partial implementation. We can
+ * only add a result, and recover the last result
  */
 public class Sample {
-  /**
-   * The animal specie for the sample
-   */
-  private Specie specie;
 
-  /**
-   * This analysis to be made on the sample
-   */
-  private Analysis analysis;
+    /**
+     * The animal specie for the sample
+     */
+    private Specie specie;
 
-  /**
-   * The order that corresponds to the sample
-   */
-  private Order order;
+    /**
+     * This analysis to be made on the sample
+     */
+    private Analysis analysis;
 
-  /**
-   * The results of analyses
-   */
-  private List<Result> results;
+    /**
+     * The order that corresponds to the sample
+     */
+    private Order order;
 
-  /**
-   * Factory for unique ids
-   */
-  private static UniqueIdSample idFactory;
+    /**
+     * The results of analyses
+     */
+    private List<Result> results;
 
-  /**
-   * Id number for the sample.
-   * The id must be unique.
-   * It is assigned during sample creation
-   */
-  private int idSample;
+    /**
+     * Factory for unique ids
+     */
+    private static UniqueIdSample idFactory;
 
-  public Sample(Analysis theAnalysis, Specie theSpecie, Order theOrder) {
-	if (idFactory==null) idFactory = new UniqueIdSample();
-	
-	idSample = idFactory.newNumber();
-	analysis = theAnalysis;
-	specie = theSpecie;
-	order = theOrder;
-	results = new ArrayList<Result>();
-  }
+    /**
+     * Id number for the sample. The id must be unique. It is assigned during
+     * sample creation
+     */
+    private int idSample;
 
-  public final int getIdSample() {
-    return idSample;
-  }
+    public Sample(Analysis theAnalysis, Specie theSpecie, Order theOrder) {
+        if (idFactory == null) {
+            idFactory = new UniqueIdSample();
+        }
 
-  public void addResult(Result theResult) {
-	results.add(theResult);	
-  }
+        idSample = idFactory.newNumber();
+        analysis = theAnalysis;
+        specie = theSpecie;
+        order = theOrder;
+        results = new ArrayList<Result>();
+    }
 
-  /**
-   * Returns the last result of the sample, as the toString function of the result
-   * Returns the null String if no result available
-   */
-  public String getResult() {
-    // Bouml preserved body begin 00025982
-       if (results.isEmpty()) return "";
-       else return results.get(results.size()-1).toString();
-    // Bouml preserved body end 00025982
-  }
+    public final int getIdSample() {
+        return idSample;
+    }
 
-  public final List<Result> getResults() {
-    return results;
-  }
+    public void addResult(Result theResult) {
+        results.add(theResult);
+    }
 
-  public final Analysis getAnalysis() {
-    return analysis;
-  }
+    /**
+     * Returns the last result of the sample, as the toString function of the
+     * result Returns the null String if no result available
+     */
+    public String getResult() {
+        // Bouml preserved body begin 00025982
+        if (results.isEmpty()) {
+            return "";
+        } else {
+            return results.get(results.size() - 1).toString();
+        }
+        // Bouml preserved body end 00025982
+    }
 
-  public final Order getOrder() {
-    return order;
-  }
+    public final List<Result> getResults() {
+        return results;
+    }
 
-  /**
-   * States if the sample has been completely analysed :
-   * 	- the result is known and validated
-   * 	- or, after three attempts, it is not readable
-   */
-  public boolean isCompleted() {
-    if (results.isEmpty()) return false;
-    Result currentResult = results.get(results.size()-1);
-    ResultStatus status = currentResult.getStatus();
-    if (status==ResultStatus.VALIDATED) return true;
-    if (status==ResultStatus.UNREADABLE && results.size()>=3) return true;
-    return false;
-  }
+    public final Analysis getAnalysis() {
+        return analysis;
+    }
 
+    public final Order getOrder() {
+        return order;
+    }
+
+    /**
+     * States if the sample has been completely analysed : - the result is known
+     * and validated - or, after three attempts, it is not readable
+     */
+    public boolean isCompleted() {
+        if (results.isEmpty()) {
+            return false;
+        }
+        Result currentResult = results.get(results.size() - 1);
+        ResultStatus status = currentResult.getStatus();
+        if (status == ResultStatus.VALIDATED) {
+            return true;
+        }
+        if (status == ResultStatus.UNREADABLE && results.size() >= 3) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        String id = ""+idSample+" "+this.getResult();
+        return id;
+    }
 }
